c14f8a6d929bbdb58816ebd3bb9ed74d9d3482fe
zoltanszikszo <szikszo.zoltan@gmail.com>
2022-01-31 16:48:38 +0100

    Calculates created

--- src/components/ShareList.js
+++ src/components/ShareList.js
@@ -15,8 +15,25 @@ function ShareList() {
       console.log(shares);
   };
 
+  let calculateShares = () => {
+      for(let i = 0; i<shares.length; i++){
+      console.log(`Share: ${shares[i].share}`)
+      console.log(`Company: ${shares[i].company}`)
+      console.log(`Price: ${shares[i].price}`)
+      console.log(`Last year dividend: ${shares[i].dividendHistory[0].dividend}`)
+      console.log(`Dividend yield-%: ${(shares[i].dividendHistory[0].dividend / shares[i].price * 100).toFixed(1)}`)
+      console.log('5-year average dividend yield-%: ' + ((shares[i].dividendHistory[0].dividend + shares[i].dividendHistory[1].dividend + shares[i].dividendHistory[2].dividend + shares[i].dividendHistory[3].dividend + shares[i].dividendHistory[4].dividend) / 5 / shares[i].price * 100).toFixed(1))
+      console.log(`5-year weighted average dividend yield-%: ${shares[i].dividendHistory[0].dividend / shares[i].price * 100}`)
+    }
+  }
+
   return (
     <div>
+      <h1>Share List</h1>
+      <h3>Jannek Christensen & Zoltán Szikszó</h3>
+      <p>
+        {calculateShares()}
+      </p>
     </div>
   );
 }
